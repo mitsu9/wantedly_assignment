812f4d7ff7da21e12f59153097f9fa4d69da7e31
mitsu9 <mitsunobu.h.9@gmail.com>
2017-02-17 17:49:39 +0900

    Make controller return only json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :skills
-  resources :users
+
+  namespace :api, { format: 'json' } do
+    namespace :v1 do
+      resources :skills
+      resources :users
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
