b873507f95f659862044a659a6cc855da9d2e005
mitsu9 <mitsunobu.h.9@gmail.com>
2017-02-19 11:09:43 +0900

    Add skill from a form

--- app/assets/javascripts/user_controller.js
+++ app/assets/javascripts/user_controller.js
@@ -1,18 +1,48 @@
 (function (){
-  angular.module('App', [])
-  .controller('userCtrl', function ($scope, $http) {
-    var id = location.pathname.match(/[1-9]+/);
-    var $uri = 'http://127.0.0.1:3000/api/v1/users/' + id + '/skills';
-    $http({
-        method : 'GET',
-        url : $uri
-    }).
-    then(function onSuccess(response) {
-      $scope.skills = response.data;
-      console.log(response);
-    }, function onError(response) {
-      console.log(response);
-    });
-  });
+  var app = angular.module('App', [])
+  app.factory('SkillService', function($http) {
+    function updateSkill(id) {
+      var $uri = 'http://127.0.0.1:3000/api/v1/users/' + id + '/skills';
+      return $http({
+          method : 'GET',
+          url : $uri
+      });
+    }
 
+    function addSkill(id, name) {
+      var $uri = 'http://127.0.0.1:3000/api/v1/users/' + id + '/skills/new';
+      var params = {}
+      params.skill = {"name": name}
+      return $http({
+          method : 'POST',
+          url : $uri,
+          data: params
+      });
+    }
+
+    return {
+      update: function(id) {
+        return updateSkill(id);
+      },
+      add: function(id, name) {
+        return addSkill(id, name);
+      }
+    }
+  });
+  app.controller('userCtrl', function($scope, SkillService) {
+    // initialize
+    var id = location.pathname.match(/[1-9]+/);
+    SkillService.update(id).then(function onSuccess(res){
+      $scope.skills = res.data;
+    });
+
+    $scope.addSkill = function(){
+      console.log("added")
+      SkillService.add(id, $scope.skill).then(function onSuccess(res){
+        SkillService.update(id).then(function onSuccess(res){
+          $scope.skills = res.data;
+        })
+      });
+    };
+  });
 })();
