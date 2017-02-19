451c25204a786205e88ecaec9e946385daa0aa38
mitsu9 <mitsunobu.h.9@gmail.com>
2017-02-19 15:44:23 +0900

    Add methods to login

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,11 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'users'      => 'users#index'
-  get 'users/:id'  => 'users#show'
-  get 'skills'     => 'skills#index'
-  get 'skills/:id' => 'skills#show'
+  get 'users'        => 'users#index'
+  get 'users/:id'    => 'users#show'
+  post 'users/login' => 'users#login'
+  get 'skills'       => 'skills#index'
+  get 'skills/:id'   => 'skills#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
