a5d823dd6eaade75c2e483e8cca6c3adc4710cd9
mitsu9 <mitsunobu.h.9@gmail.com>
2017-02-19 19:38:32 +0900

    Fix validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,9 @@ class Like < ActiveRecord::Base
   belongs_to :user_skill
   belongs_to :user
 
-  validates :user_skill, :user, uniqueness: true
+  validates :user_skill,
+    uniqueness: {
+      message: "既にlikeしています",
+      scope: [:user]
+    }
 end
